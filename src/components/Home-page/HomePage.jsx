4ecd159173ab3c8ed5b34af40835dae045c676f2
vstarvrajesh <109358855+vstarvrajesh@users.noreply.github.com>
2023-06-29 11:22:22 +0530

    Commit

---
+++ src/components/Home-page/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../css/homepage/homepage.css'
+
+export const HomePage = () => {
+    return (
+        <div className='HomePage'>
+
+        </div>
+    )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../css/navbar/navbar.css'
+
+export const Navbar = () => {
+    return (
+        <div className='Navbar'>
+
+        </div>
+    )
+}
